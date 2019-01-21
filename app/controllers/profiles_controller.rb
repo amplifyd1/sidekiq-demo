ea036849d2687f8a61db9a4e920832a4e4ca8252
wesley sprague <wesleysprague@yahoo.com>
2019-01-21 13:36:24 -0500

    created user.rake file with to update users

---
+++ app/controllers/profiles_controller.rb
@@ -0,0 +1,18 @@
+class ProfilesController < ApplicationController
+  def update
+    if current_user.update(user_params)
+      UpdateUserJob.perform_later(current_user.id)  # <- The job is queued
+      flash[:notice] = "Your profile has been updated"
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.permit(:user_id, :email)
+    # Some strong params of your choice
+  end
+end
